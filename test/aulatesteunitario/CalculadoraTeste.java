2ae85b64d6631e85682fb0e52ab6491229020efc
ANDERSON DOS SANTOS NEVES <ASNEVES2@UNIVEL.UNIVEL.BR>
2023-04-05 22:03:08 -0300

    Teste

--- test/aulatesteunitario/CalculadoraTeste.java
+++ test/aulatesteunitario/CalculadoraTeste.java
@@ -1,6 +1,8 @@
 package aulatesteunitario;
 
 //import org.junit.Assert;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import junit.framework.Assert;
 //import org.junit.Assert;
 import org.junit.Test;
@@ -20,9 +22,14 @@ public class CalculadoraTeste {
     
     @Test
     public void testarRaizQuadrada(){
-        
-        Assert.assertEquals(9.0, Calculadora.raiQuadrada(81));
-        Assert.assertEquals(-4.0, Calculadora.raiQuadrada(-4));
+        try {
+            Assert.assertEquals(Exception.class, Calculadora.raiQuadrada(-4));
+            Assert.assertEquals(9.0, Calculadora.raiQuadrada(81));
+            Assert.assertEquals(-4.0, Calculadora.raiQuadrada(-4));
+        } catch (Exception ex) {
+            //Logger.getLogger(CalculadoraTeste.class.getName()).log(Level.SEVERE, null, ex);
+            Assert.assertEquals("Não há raiz quadrada de negativos", this);
+        }
     }
     
     
