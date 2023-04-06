4e7e275977b0d8725db518289f0cc7d4e4931f87
Anderson Neves <neves1697@gmail.com>
2023-04-06 20:04:29 -0300

    first commit

--- src/aulatesteunitario/Calculadora.java
+++ src/aulatesteunitario/Calculadora.java
@@ -10,7 +10,7 @@ public class Calculadora {
     
     public static double raiQuadrada(double x) throws Exception{
         if(x < 0){
-            throw new Exception("Não há raiz quadrada com negativos");
+            throw new ExceptionRaizQuadradaNegativos("Não há raiz quadrada com negativos");
         }
         return Math.sqrt(x);
     }
---
+++ src/aulatesteunitario/ExceptionRaizQuadradaNegativos.java
@@ -0,0 +1,12 @@
+package aulatesteunitario;
+
+public class ExceptionRaizQuadradaNegativos extends Exception {
+
+    public ExceptionRaizQuadradaNegativos() {
+    }
+    
+    public ExceptionRaizQuadradaNegativos(String message){
+        super(message);
+    }
+
+}
